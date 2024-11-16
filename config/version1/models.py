816d48cd48578e92fbde8f5289f9e007a054f42a
FEUGANG KOUAKEM Ange Corianne <afeugangkouake@et.intechinfo.fr>
2024-11-16 07:37:54 +0100

    structure de base

---
+++ config/version1/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+class File(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file = models.FileField(upload_to='/uploads')
+    encrypted_password = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
